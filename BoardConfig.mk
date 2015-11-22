8f79126fa4919d58fcd02c9e2d7ef1de1b6d4c69
JohnHoder <hoder.john@gmail.com>
2015-11-22 01:10:05 +0100

    Initial commit (Cloned)

---
+++ BoardConfig.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
+
+## TWRP
+DEVICE_RESOLUTION := 240x320
+
+include device/samsung/msm7x27-common/BoardConfigCommon.mk
+
+## Kernel, bootloader
+TARGET_BOOTLOADER_BOARD_NAME := tass
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+	TARGET_KERNEL_CONFIG := cyanogenmod_tass_defconfig
+else
+	TARGET_KERNEL_CONFIG := cyanogenmod_tass_recovery_defconfig
+endif
+TARGET_OTA_ASSERT_DEVICE := tass,GT-S5570
+
+## Recovery
+BOARD_LDPI_RECOVERY := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
+
+## Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/tass/bluetooth
+
+## Camera
+TARGET_PREBUILT_LIBCAMERA := false
+TARGET_CAMERA_SENSOR_MP_SIZE := 3
+
---
+++ device_tass.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Note: this file and all of its includes are parsed before any
+## BoardConfig files. 3.0 Kernel configuration must be set here.
+
+## Build the 3.0.x Kernel
+#BUILD_WITH_30X_KERNEL := false
+
+## Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/tass/ramdisk/TASS.rle:root/TASS.rle
+
+# Inherit products (Most specific first)
+# tass blobs > samsung common(device/vendor) > other blobs
+$(call inherit-product, vendor/samsung/tass/vendor_blobs.mk)
+$(call inherit-product, device/samsung/msm7x27-common/common.mk)
+$(call inherit-product, vendor/samsung/msm7x27-common/vendor.mk)
+
+## LDPI assets
+PRODUCT_AAPT_CONFIG := normal mdpi ldpi
+PRODUCT_AAPT_PREF_CONFIG := ldpi
+$(call inherit-product, device/ldpi-common/ldpi.mk)
+
+## Inherit overlays  (Most specific last)
+DEVICE_PACKAGE_OVERLAYS += device/samsung/tass/overlay
